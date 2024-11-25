d8994e373bb7be3d5fb61a8c434de33ffaf36e94
yurri2506 <22520590@gm.uit.edu.vn>
2024-11-25 14:37:32 +0700

    update login logout

--- src/redux/slices/userSlice.js
+++ src/redux/slices/userSlice.js
@@ -22,36 +22,15 @@ const userSlice = createSlice({
         isAuthenticated: true,
       };
 
-      // Lưu accessToken và refreshToken vào localStorage
-      if (action.payload.access_token) {
-        localStorage.setItem("access_token", action.payload.access_token);
-      }
-      if (action.payload.refreshToken) {
-        localStorage.setItem("refreshToken", action.payload.refreshToken);
-      }
-
       return updatedState;
     },
     resetUser: (state) => {
-      // Chỉ xóa thông tin người dùng, giữ lại token
       return {
-        ...state,
-        user_name: "",
-        user_email: "",
-        user_phone: "",
-        user_address: "",
-        user_avt_img: "",
-        isAuthenticated: false,
+        initialState
       };
-    },
-    logout: () => {
-      // Xóa toàn bộ thông tin và token khỏi state và localStorage
-      localStorage.removeItem("access_token");
-      localStorage.removeItem("refreshToken");
-      return initialState;
     },
   },
 });
 
-export const { updateUser, resetUser, logout } = userSlice.actions;
+export const { updateUser, resetUser } = userSlice.actions;
 export default userSlice.reducer;
